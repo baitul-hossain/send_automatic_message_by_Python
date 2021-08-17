0f45ab0a2b12cf187222ae4a03c64f006a9d498d
Baitul Hossain <88625903+baitul-hossain@users.noreply.github.com>
2021-08-17 02:42:46 +0100

    updated

---
+++ main.py
@@ -0,0 +1,18 @@
+# Install PyAutoGui -- pip install PyAutoGUI
+
+import pyautogui as pag
+import time
+
+while True:
+    time.sleep(3)
+    pag.typewrite("Hello, Python says..how are you?")
+    pag.press('Enter')
+
+# To send automatic message put your mouse cursor
+# on the text field of the messenger and run this
+# python script.
+
+
+
+
+
